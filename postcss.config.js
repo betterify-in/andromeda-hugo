3781a31b7d8872e665ff50e2b8092abe8ef99f90
betterify <116065586+betterify-in@users.noreply.github.com>
2023-06-11 00:53:07 +0530

    Local changes

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,5 @@
 const purgecss = require("@fullhuman/postcss-purgecss")({
-    content: ["./hugo_stats.json",
-              "index.html",
-  ],
+    content: ["./hugo_stats.json"],
     defaultExtractor: (content) => {
       const els = JSON.parse(content).htmlElements;
       return [...(els.tags || []), ...(els.classes || []), ...(els.ids || [])];
